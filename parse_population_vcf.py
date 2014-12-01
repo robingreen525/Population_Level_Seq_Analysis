afe9818c0c3c0124ff0129b44c50a0983a7c2947
Robin Green <rjgreen@rhino02.fhcrc.org>
2014-12-01 15:51:25 -0800

    First Commit of RJG Pop Level Seq Code

---
+++ parse_population_vcf.py
@@ -0,0 +1,14 @@
+import sys
+
+f=open(sys.argv[1],'r')
+
+ll=f.readlines()
+
+for line in ll:
+	line=line.split()
+	if line[0][0]=='S' or line[0][0]=='c':
+		#print line[0],line[1],line[3],line[4]
+		info=line[7].split(';')
+		#print info[0],info[5],info[7]
+		prop=info[0].split('=')[1]
+		print line[0],line[1],line[3],line[4],prop
