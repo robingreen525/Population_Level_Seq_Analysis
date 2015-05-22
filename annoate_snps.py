e14d542d2eb611ccb6bebea3086f9bb20e3b0a39
Robin Green <rjgreen@rhino04.fhcrc.org>
2015-05-22 01:17:28 -0700

    prelim version of amino acid calling for snps, need to verify correctness. Only for watson strand

--- annoate_snps.py
+++ annoate_snps.py
@@ -2,6 +2,13 @@
 # along with what codon and position in the codon. This information can be used to determine which frame 
 # the mutation is in and predict amino acid substitutions  
 
+
+#usage:
+# python Population_Level_Seq_Analysis/annoate_snps.py -genefile /fh/fast/shou_w/NextGenSeq/reference/S288C.gff -strainfile /fh/fast/shou_w/NextGenSeq/PopSeq/Parameter+Strain_Files/AaronMiller_strains.txt -d /fh/fast/shou_w/NextGenSeq/PopSeq/MDunham/Miller_SulfateGlucosePhosphate_2015/
+# works if in /fh/fast/shou_w/NextGenSeq/PopSeq/MDunham/Miller_SulfateGlucosePhosphate_2015
+
+
+
 #import libraries needed
 import sys
 import os
@@ -14,6 +21,9 @@ parser.add_argument('-strainfile', type=argparse.FileType('r'))
 parser.add_argument('-dir', dest='dir')
 
 args = parser.parse_args()
+
+# get the path where the data are stored. will be used with other strings later
+path=args.dir
 
 
 strains=[]
@@ -47,4 +57,41 @@ for line in ll:
 			if 'gene' in globals():	## see if gene name was assigned along with systematic name
 				lookup_table.append([chr,start,end,strand,ID,gene])
 			else:
-				lookup_table.append([chr,start,end,strand,ID,'unknown'])
+				lookup_table.append([chr,start,end,strand,ID,'unknown']) # if no standard name, just add 'unknown'
+
+# for each strain.parsed.txt file, annotate snp calls (gene, mutation, aa change?) and write to
+# strain.parsed.annotated.txt file
+
+
+
+for strain in strains:
+	
+	full_path=path+strain
+	file=full_path+'/'+strain+'.parsed.txt'
+	newfile=full_path+'/'+strain+'.annotated.parsed.txt'
+	print(file)
+	if(os.path.exists(file)):
+		file=open(file,'r')
+		newfile=open(newfile,'w')
+		ll=file.readlines()
+		for line in ll:  #for each snp, find where it is in the lookup_table
+			line=line.split(',')
+			if(len(line)>1):
+				chr=line[0] #chromosome
+				pos=int(line[1]) # position
+				for gene in lookup_table:
+					chr_ref=gene[0] #chromosome of each snp in lookup table
+					start_pos_ref=int(gene[1]) # starting position (from the Waston strand (+))
+					end_pos_ref=int(gene[2])  # ending position (from the Watson strand (+))
+					if(chr_ref==chr):
+						if(start_pos_ref<=pos):
+							if(end_pos_ref>=pos): ## if snp found within gene
+								ref_reads=float(line[3])
+								alt_reads=float(line[len(line)-1].rstrip('\n'))
+								freq=(float (alt_reads/(alt_reads+ref_reads)))
+								string=line[0]+','+line[1]+','+line[2]+','+line[3]+','+line[4]+','+line[5].rstrip('\n')+','+gene[3]+','+gene[4]+','+gene[5]+','+str(freq)+'\n'
+								newfile.write(string)
+								#newfile.write(line[0],line[1],line[2],line[3],line[4],line[5].rstrip('\n'), gene[3],gene[4],gene[5],freq,'\n')
+	
+	file.close()
+	newfile.close()							
---
+++ record_aminoacid_changes.py
@@ -0,0 +1,222 @@
+# the purpose of this script is to determine if the observed SNPS will result in an amino acid substitution.
+# i have a fasta file that gives the sequence for each gene in the cerviesea genome (from SGD). I will build a dictionary
+# of the genes and use that information to assess frame and mutations for each SNP called.
+
+
+
+# Dictionary holding the code to life: Codon to Amino Acid map
+code = {     'ttt': 'F', 'tct': 'S', 'tat': 'Y', 'tgt': 'C',
+             'ttc': 'F', 'tcc': 'S', 'tac': 'Y', 'tgc': 'C',
+             'tta': 'L', 'tca': 'S', 'taa': '*', 'tga': '*',
+             'ttg': 'L', 'tcg': 'S', 'tag': '*', 'tgg': 'W',
+             'ctt': 'L', 'cct': 'P', 'cat': 'H', 'cgt': 'R',
+             'ctc': 'L', 'ccc': 'P', 'cac': 'H', 'cgc': 'R',
+             'cta': 'L', 'cca': 'P', 'caa': 'Q', 'cga': 'R',
+             'ctg': 'L', 'ccg': 'P', 'cag': 'Q', 'cgg': 'R',
+             'att': 'I', 'act': 'T', 'aat': 'N', 'agt': 'S',
+             'atc': 'I', 'acc': 'T', 'aac': 'N', 'agc': 'S',
+             'ata': 'I', 'aca': 'T', 'aaa': 'K', 'aga': 'R',
+             'atg': 'M', 'acg': 'T', 'aag': 'K', 'agg': 'R',
+             'gtt': 'V', 'gct': 'A', 'gat': 'D', 'ggt': 'G',
+             'gtc': 'V', 'gcc': 'A', 'gac': 'D', 'ggc': 'G',
+             'gta': 'V', 'gca': 'A', 'gaa': 'E', 'gga': 'G',
+             'gtg': 'V', 'gcg': 'A', 'gag': 'E', 'ggg': 'G'
+        }
+
+#import libraries needed
+import sys
+import os
+import argparse
+
+#define parsing options
+parser = argparse.ArgumentParser()
+parser.add_argument('-genefasta',type=argparse.FileType('r'))
+parser.add_argument('-strainfile', type=argparse.FileType('r'))
+parser.add_argument('-dir', dest='dir')
+parser.add_argument('-genefile',type=argparse.FileType('r'))
+
+args = parser.parse_args()
+
+# get the path where the data are stored. will be used with other strings later
+path=args.dir
+
+
+strains=[]
+### use the strain file flag to get the list of strains to analyze
+ll=args.strainfile.readlines()
+for line in ll:
+	line=line.split('\t')
+	strains.append(line[0])
+	
+
+# build a lookup table of all genes in cerevisea genome	
+ll=args.genefile.readlines()	
+lookup_table=[] # will be a list of lists where each sublist is the gene information
+
+
+for line in ll:
+	f=line[0:3] #first three letters of line, want to get 'chr' starting lines in .gff file
+	if(f =='chr'):
+		line=line.split()
+		if(line[2]=='gene'):
+			chr=line[0] #chromosome
+			start=line[3] # start site (relative to plus strand)
+			end=line[4] # end site (relative to plus strand)
+			strand=line[6] # what strand, if not plus, start and end will be flipped later
+			info=line[8].split(';') # where the ID and gene name information is
+			for thing in info:
+				thing=thing.split('=')
+				if(thing[0]=='ID'): ID=thing[1] # systematic name
+				elif(thing[0]=='gene'):  # common/standard name
+					gene=thing[1]
+					
+			if 'gene' in globals():	## see if gene name was assigned along with systematic name
+				lookup_table.append([chr,start,end,strand,ID,gene])
+			else:
+				lookup_table.append([chr,start,end,strand,ID,'unknown']) # if no standard name, just add 'unknown'
+
+
+
+
+
+gene_seq={}
+
+### build a dictionary of genes and their sequences
+ll=args.genefasta.readlines()
+end=len(ll)
+i=0
+
+while i<end-1:
+	line=ll[i]
+	if line[0]=='>': # for each header, get the corresponding seqeunce for each gene
+		line=line.split()
+		sysname=line[0][1:] # get the systematic name
+		geneseq=''
+		i+=1
+		newline=ll[i]
+		while newline[0]!='>' and i<end-1: # keep going until you reach another header line
+			geneseq=geneseq+newline.rstrip('\n')
+			i+=1
+			newline=ll[i]
+		gene_seq[sysname]=geneseq # assign seq to key (systematic name)
+	
+	
+all_mutations=path+'/master.csv'	
+a=open(all_mutations,'w')	
+	
+for strain in strains:
+	full_path=path+strain
+	file=full_path+'/'+strain+'.annotated.parsed.txt'
+	
+	print(file)
+	if os.path.exists(file):
+		f=open(file)
+		ll=f.readlines() # open sample.annotated.parsed.txt file
+		for line in ll:
+			line=line.split(',')
+			sys_gene=line[7] # get systamtic name for gene where snp is
+			seq=gene_seq[sys_gene] # get the sequence of the gene
+			for gene in lookup_table: # find the start,end, and strand  for the gene being analyzed
+				sys_gene_ref=gene[4]
+				if sys_gene_ref==sys_gene: # if the gene being analyzed is found, get positional information
+					start=int(gene[1])
+					end=int(gene[2])
+					strand=gene[3]
+					pos=int(line[1])
+					
+					#print sys_gene,sys_gene_ref,start, end, strand
+					#print(strand)
+					if(strand=='+'):
+						frame=((pos-start)%3) # 1=first base, 2=second base, 0= third base
+						
+						if(frame==1):
+							codon=seq[(start-start):(pos-start)+3]
+							codon=codon[-3:]
+							newcodon=list(codon)
+							newcodon[0]=line[4]
+							newcodon=''.join(newcodon)
+							#print(line)
+							#print codon
+							#print newcodon
+							codon=codon.lower()
+							newcodon=newcodon.lower()
+							
+							if(len(codon)==3 and len(newcodon)==3): #would miss indels, fix later
+								aa_ref=code[codon]
+								aa_new=code[newcodon]
+							
+								if(aa_ref!=aa_new):
+									#print 'Nonsynamous mutation'
+									#print aa_ref
+									#print aa_new
+									data=strain+','
+									
+									for thing in line:
+										data=data+thing.strip('\n')+','
+									
+									data=data+','+aa_ref+str(len(seq)/3)+aa_new+'\n'
+									#print(data)
+									a.write(data)
+									
+								
+						elif(frame==2):
+							#codon=seq[(start-start):(pos-start)+3]
+							codon=seq[(start-start):(pos-start)+2]
+							codon=codon[-3:]
+							newcodon=list(codon)
+							newcodon[0]=line[4]
+							newcodon=''.join(newcodon)
+							#print(line)
+							#print codon
+							#print newcodon
+							codon=codon.lower()
+							newcodon=newcodon.lower()
+							
+							if(len(codon)==3 and len(newcodon)==3): #would miss indels, fix later
+								aa_ref=code[codon]
+								aa_new=code[newcodon]
+							
+								if(aa_ref!=aa_new):
+									#print 'Nonsynamous mutation'
+									#print aa_ref
+									#print aa_new
+									data=strain+','
+									
+									for thing in line:
+										data=data+thing.strip('\n')+','
+									
+									data=data+','+aa_ref+str(len(seq)/3)+aa_new+'\n'
+									#print(data)
+									a.write(data)
+						elif(frame==0):
+							#codon=seq[(start-start):(pos-start)+3]
+							codon=seq[(start-start):(pos-start)+1]
+							codon=codon[-3:]
+							newcodon=list(codon)
+							newcodon[0]=line[4]
+							newcodon=''.join(newcodon)
+							#print(line)
+							#print codon
+							#print newcodon
+							codon=codon.lower()
+							newcodon=newcodon.lower()
+							
+							if(len(codon)==3 and len(newcodon)==3): #would miss indels, fix later
+								aa_ref=code[codon]
+								aa_new=code[newcodon]
+							
+								if(aa_ref!=aa_new):
+									#print 'Nonsynamous mutation'
+									#print aa_ref
+									#print aa_new
+									data=strain+','
+									
+									for thing in line:
+										data=data+thing.strip('\n')+','
+									
+									data=data+','+aa_ref+str(len(seq)/3)+aa_new+'\n'
+									#print(data)
+									a.write(data)	
+
+a.close()							
+		
